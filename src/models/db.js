7d687e91ab54f7403a59324f1873eecec5ffd1c7
Otávio Santiago <otaviosatago@gmail.com>
2025-01-31 11:22:26 -0300

    Melhorei a Segurança

--- src/models/db.js
+++ src/models/db.js
@@ -1,9 +1,15 @@
 // importações
 const { Sequelize } = require('sequelize')
+require('dotenv').config({path:'.env'})
+
+const user = process.env.DB_USER
+const pass = process.env.DB_PASS
+const host = process.env.DB_HOST
+const database = process.env.DB_DATABASE
 
 // config
-const sequelize = new Sequelize('SitePartituras', 'root', 'P@cienc1a', {
-    host: '147.93.68.67',
+const sequelize = new Sequelize(database, user, pass, {
+    host: host,
     dialect: 'mysql',
     logging: false
 })
